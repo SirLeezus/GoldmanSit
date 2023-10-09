f78017248ba8d95a7a878ce77ad33642cf6536e8
Lee <leecook808@gmail.com>
2023-10-09 13:20:12 -0700

    fixes

--- src/main/java/lee/code/sit/listeners/SitListener.java
+++ src/main/java/lee/code/sit/listeners/SitListener.java
@@ -4,11 +4,7 @@ import lee.code.sit.lang.Lang;
 import lee.code.sit.nms.Chair;
 import net.kyori.adventure.text.Component;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.level.block.CarpetBlock;
-import net.minecraft.world.level.block.WoolCarpetBlock;
-import org.bukkit.Location;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Bisected;
 import org.bukkit.block.data.type.Slab;
 import org.bukkit.block.data.type.Stairs;
@@ -28,7 +24,6 @@ public class SitListener implements Listener {
     if (!e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) return;
     if (!e.getPlayer().getInventory().getItemInMainHand().getType().isAir()) return;
     final Block block = e.getClickedBlock();
-    final Player player = e.getPlayer();
     if (block == null) return;
     if (!(block.getBlockData() instanceof Stairs) && !(block.getBlockData() instanceof Slab) && !block.getType().name().endsWith("CARPET")) return;
     if (block.getBlockData() instanceof Stairs stairs && stairs.getHalf().equals(Bisected.Half.TOP)) return;
@@ -39,7 +34,7 @@ public class SitListener implements Listener {
           final Component name = vehicle.customName();
           if (name != null && name.equals(Component.text("chair"))) {
             e.setCancelled(true);
-            player.sendActionBar(Lang.ERROR_CHAIR_OCCUPIED.getComponent(null));
+            e.getPlayer().sendActionBar(Lang.ERROR_CHAIR_OCCUPIED.getComponent(null));
             return;
           }
         }
@@ -50,6 +45,6 @@ public class SitListener implements Listener {
     final ServerLevel world = ((CraftWorld) block.getWorld()).getHandle();
     world.addFreshEntity(chair);
     final Entity entityChair = chair.getBukkitEntity();
-    entityChair.addPassenger(player);
+    entityChair.addPassenger(e.getPlayer());
   }
 }
